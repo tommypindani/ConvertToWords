edca74ce63e9f599afa2ed9b8e82c14eb0da6b48
Brendon Pindani <TommyPindani@gmail.com>
2020-09-17 02:33:40 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace ConvertToWords
+{
+    class NumberToWords
+    {
+        private static String[] units = { "Zero", "One", "Two", "Three",
+    "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven",
+    "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen",
+    "Seventeen", "Eighteen", "Nineteen" };
+        private static String[] tens = { "", "", "Twenty", "Thirty", "Forty",
+    "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+
+        public static String ConvertAmount(double amount)
+        {
+            try
+            {
+                Int64 amount_int = (Int64)amount;
+                Int64 amount_dec = (Int64)Math.Round((amount - (double)(amount_int)) * 100);
+                if (amount_dec == 0)
+                {
+                    return Convert(amount_int) + " Rands " +  " Only.";
+                }
+                else
+                {
+                    return Convert(amount_int) + " Point " + Convert(amount_dec) + " Only.";
+                }
+            }
+            catch (Exception e)
+            {
+                
+            }
+            return "";
+        }
+
+        public static String Convert(Int64 i)
+        {
+            if (i < 20)
+            {
+                return units[i];
+            }
+            if (i < 100)
+            {
+                return tens[i / 10] + ((i % 10 > 0) ? " " + Convert(i % 10) : "");
+            }
+            if (i < 1000)
+            {
+                return units[i / 100] + " Hundred"
+                        + ((i % 100 > 0) ? " And " + Convert(i % 100) : "");
+            }
+            if (i < 100000)
+            {
+                return Convert(i / 1000) + " Thousand "
+                + ((i % 1000 > 0) ? " " + Convert(i % 1000) : "");
+            }
+          
+            return Convert(i / 1000000000) + "  "
+                    + ((i % 1000000000 > 0) ? " " + Convert(i % 1000000000) : "");
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter a Number to convert to words");
+                string number = Console.ReadLine();
+                number = ConvertAmount(double.Parse(number));
+
+                Console.WriteLine("Number in words is \n{0}", number);
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
